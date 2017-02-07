3800981c36b7a7df67cd3f5ea6de3538db3be7ef
Chrimm <ripitrust@gmail.com>
2017-02-07 14:04:47 +0800

    change app name

--- application.py
+++ application.py
@@ -1,30 +1,30 @@
 from flask import Flask , render_template
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/index')
+@application.route('/index')
 def home_page():
     return render_template('index.html')
 
 
-@app.route('/events')
+@application.route('/events')
 def event_page():
     return render_template('events.html')
 
 
 
-@app.route('/foods')
+@application.route('/foods')
 def food_page():
     return render_template('foods.html')
 
 
-@app.route('/places')
+@application.route('/places')
 def place_page():
     return render_template('places.html')
 
 
-@app.route('/test')
+@application.route('/test')
 def experiment_page():
 
     return render_template('experiment.html')
@@ -33,4 +33,4 @@ def experiment_page():
 
 if __name__ == '__main__':
 
-    app.run(host="0.0.0.0", port=20000, debug=True)
+    application.run(host="0.0.0.0", port=20000, debug=True)
